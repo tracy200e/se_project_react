ee5bf027596f021118e76ec44f7433f34df0d249
Tracy Chen <tracy200e@gmail.com>
2023-08-24 14:26:23 +1000

    build: created modal with form's template

--- src/componenets/App/App.js
+++ src/componenets/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
 
 function App() {
   const weatherTemp = '75°F';
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Main weatherTemp={ weatherTemp } />
       <Footer />
+      <ModalWithForm title='New Garment'>These are the children</ModalWithForm>
     </div>
   );
 }
---
+++ src/componenets/ModalWithForm/ModalWithForm.js
@@ -0,0 +1,23 @@
+import './ModalWithForm.css';
+
+const ModalWithForm = ({ 
+    children, 
+    buttonText = 'Add garment', 
+    title,
+    onClose,
+    name,
+}) => {
+    console.log('ModalWithForm')
+    return (
+        <div className={`modal modal_type_${name}`}>
+            <div className='modal_content'>
+            <button type='button' onclick={ onClose } />
+            <h3>{ title }</h3>
+            <form>{ children }</form>
+            <button type='submit'>{ buttonText }</button>
+            </div>
+        </div>
+    );
+};
+
+export default ModalWithForm;
