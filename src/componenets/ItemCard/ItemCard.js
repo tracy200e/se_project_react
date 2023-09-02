c03db98c0a3406a08bbc187477bb1503f3a07937
Tracy Chen <tracy200e@gmail.com>
2023-09-02 15:33:14 +1000

    build: card name and like button positions

--- src/componenets/ItemCard/ItemCard.js
+++ src/componenets/ItemCard/ItemCard.js
@@ -1,10 +1,16 @@
+import './ItemCard.css';
+import like_button from '../../images/like.svg';
+
 const ItemCard = ({ item, onSelectCard }) => {
     return <div>
-      <div>
+      <div className="card">
         <img src={item.link} className='card_image' onClick={() => onSelectCard(item)} alt='suggestion card' />
+        <div className='card_title'>
+          <div className='card_name'>{item.name}</div>
+          <img src={like_button} className='card_like' alt='like button' />
+        </div>
       </div>
-      <div className='card_name'>{item.name}</div>
-    </div>;
+    </div>
 }
 
 export default ItemCard;
