46c1ee6d3285a2570dcb42058f87e4b97502ee0a
Tracy Chen <tracy200e@gmail.com>
2023-09-06 21:21:50 +1000

    fix: close button design

--- src/componenets/ItemModal/ItemModal.js
+++ src/componenets/ItemModal/ItemModal.js
@@ -1,3 +1,5 @@
+import './ItemModal.css';
+
 const ItemModal = ({ selectedCard, onClose }) => {
 
     console.log('item modal');
@@ -5,9 +7,7 @@ const ItemModal = ({ selectedCard, onClose }) => {
     return (
         <div className={`modal`}>
           <div className='modal__content'>
-            <button type='button' onClick={onClose}>
-                Close
-            </button>
+            <button type='button' className='modal__close-button' onClick={onClose}></button>
             <img src={selectedCard.link} alt='item card' />
             <div>{selectedCard.name}</div>
             <div>Weather type: {selectedCard.weather}</div>
