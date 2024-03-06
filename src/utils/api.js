ad00578e8dad5b699edc823c3d66f2763d37f66a
Tracy Chen <tracy200e@gmail.com>
2024-03-06 19:03:26 +1100

    feat: create a local mock server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,44 @@
-const baseUrl = 'http://localhost:3001';
+const baseUrl = "http://localhost:3001";
 
+// Add item
+const addItems = () => {
+    return fetch(`${baseUrl}/items`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    .then((res) => {
+        return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    });
+};
+
+// Get item
+const getItems = () => {
+    return fetch(`${baseUrl}/items`, {
+        method: "GET",
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    .then((res) => {
+        return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    });
+};
+
+// Delete item
+const deleteItems = (id) => {
+    return fetch(`${baseUrl}/items/:${id}`, {
+        method: "DELETE",
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    .then((res) => {
+        return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    });
+};
+
+const api = { addItems, getItems, deleteItems };
+
+export default api;
