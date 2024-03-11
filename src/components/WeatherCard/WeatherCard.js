76d75daf28bb6ece7fea0214fe5e6a86e4e10f1f
Tracy Chen <tracy200e@gmail.com>
2024-03-11 21:31:09 +1100

    fix: use context for temperature unit

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,13 +1,15 @@
 import "./WeatherCard.css";
 import { weatherOptions } from "../../utils/constants";
 import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
+import { useContext } from "react";
 
-const WeatherCard = ({ day, type, weatherTemp = "", currentTemperatureUnit}) => {
+const WeatherCard = ({ day, type, weatherTemp = ""}) => {
   const imageSource = weatherOptions.filter((i) => {
     return i.day === day && i.type === type;
   });
 
   const imageSourceUrl = imageSource[0].url || "";
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
 
   return (
     <section className="weather" id="weather">
