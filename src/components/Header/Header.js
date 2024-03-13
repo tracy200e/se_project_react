f73ab3243447409339064511a82b6ca857387a0f
Tracy Chen <tracy200e@gmail.com>
2024-03-13 12:33:29 +1100

    feat: add current date and city

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,36 @@ import { Link } from "react-router-dom";
 import logo from "../../images/logo.svg";
 import avatar from "../../images/avatar.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { useState, useEffect } from "react";
+import { getForecastWeather } from "../../utils/weatherApi";
 
 const Header = ({ onCreateModal }) => {
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentCity, setCurrentCity] = useState("");
+
+  useEffect(() => {
+    const getCurrentDate = () => {
+      const date = new Date();
+      const options = {
+        month: "long",
+        day: "numeric",
+      };
+
+      return date.toLocaleDateString(undefined, options);
+    };
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      getForecastWeather()
+        .then((data) => {
+          setCurrentCity(data.name);
+        })
+        .catch((error) => console.error(`Error fetching city: ${error.status}`));
+    });
+
+    setCurrentDate(getCurrentDate());
+    setCurrentCity();
+  }, []);
+
   return (
     <header className="header">
       <div className="header__logo">
@@ -13,7 +41,9 @@ const Header = ({ onCreateModal }) => {
             <img src={logo} alt="logo" />
           </Link>
         </div>
-        <div>Date</div>
+        <div>
+          {currentDate}, {currentCity}
+        </div>
       </div>
       <div className="header__avatar-logo">
         <ToggleSwitch />
