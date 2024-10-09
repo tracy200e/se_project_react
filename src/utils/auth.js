df19c1c320b9e426b5b6a57b9cd9e6713b69f051
Tracy Chen <tracy200e@gmail.com>
2024-10-09 21:38:55 +1100

    add: registration and authorisation methods

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,43 @@
 const baseUrl = "http://localhost:3001";
 const headers = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
+export const register = ({ name, email, password, avatar }) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password, avatar }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      console.log(data);
+    });
+};
+
+export const authorize = (name, password) => {
+  return fetch(`${baseUrl}/auth/local`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, password }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.jwt) {
+        localStorage.setItem("jwt", data.jwt);
+        return data;
+      } else {
+        return;
+      }
+    });
 };
