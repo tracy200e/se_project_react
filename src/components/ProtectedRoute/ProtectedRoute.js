a019b24698286c6e1e2cf438b5f16a765df42d27
Tracy Chen <tracy200e@gmail.com>
2024-10-10 22:22:40 +1100

    update: correct format

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 const ProtectedRoute = ({ isLoggedIn, children, ...props }) => {
-    <Route {...props}>
-        {isLoggedIn ? children : <Redirect to="/login" />}
-    </Route>
-}
+  <Route {...props}>{isLoggedIn ? children : <Redirect to="/signin" />}</Route>;
+};
 
 export default ProtectedRoute;
