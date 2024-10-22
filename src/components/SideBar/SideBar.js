2cc921719509ad1dd90d77ab4a123d421b154cd4
Tracy Chen <tracy200e@gmail.com>
2024-10-22 20:56:29 +1100

    updated: profile side bar design

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,11 +1,29 @@
-import avatar from "../../images/avatar.svg";
 import "./SideBar.css";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-const SideBar = () => {
+const SideBar = ({ handleLogOut, openEditModal }) => {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <div>
       <div className="side-bar">
-        <img src={avatar} alt="profile-pic" />
+        <div className="side-bar__profile">
+          <img
+            className="side-bar__profile-avatar"
+            src={currentUser.avatar}
+            alt="profile-pic"
+          />
+          <div>{currentUser.name}</div>
+        </div>
+        <div className="side-bar__menu">
+          <div className="side-bar__menu-button" onClick={openEditModal}>
+            Change profile data
+          </div>
+          <div className="side-bar__menu-button" onClick={handleLogOut}>
+            Log out
+          </div>
+        </div>
       </div>
     </div>
   );
